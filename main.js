b1b42ddc815bfadcd5ad448401468817b5043dcd
Marc Petit <marc.petit@mataro.epiaedu.cat>
2019-03-07 12:55:16 +0100

    Changed dia to substring with only date

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ function weather() {
 
     $.getJSON("https://dataservice.accuweather.com/forecasts/v1/daily/5day/"+ciutat+"?apikey=pdjxEIPtWE2tmrCA8sJ0H5NXh7zOEl5s&language=es&metric=true",function(data) {
 
-    var dia = data["DailyForecasts"]["0"]["Date"];
+    var diallarg = data["DailyForecasts"]["0"]["Date"];
+    var dia = diallarg.substring(0,9);
     var min = data["DailyForecasts"]["0"]["Temperature"]["Minimum"]["Value"];
     var max = data["DailyForecasts"]["0"]["Temperature"]["Maximum"]["Value"];
     var estatdia = data["DailyForecasts"]["0"]["Day"]["Icon"];
@@ -22,7 +23,7 @@ function weather() {
 
     document.getElementById("estatdia0").innerHTML = estatdia;
 
-    document.getElementById("fraseestata0").innerHTML = fraseestatdia;
+    document.getElementById("fraseestatdia0").innerHTML = fraseestatdia;
 
     document.getElementById("estatnit0").innerHTML = estatnit;
 
