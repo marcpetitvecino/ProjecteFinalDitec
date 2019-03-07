629aa51961c35b2200fadf374fe97fd11fdcdd39
Marc Petit <marc.petit@mataro.epiaedu.cat>
2019-03-07 12:56:40 +0100

    Minor changes

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function weather() {
     $.getJSON("https://dataservice.accuweather.com/forecasts/v1/daily/5day/"+ciutat+"?apikey=pdjxEIPtWE2tmrCA8sJ0H5NXh7zOEl5s&language=es&metric=true",function(data) {
 
     var diallarg = data["DailyForecasts"]["0"]["Date"];
-    var dia = diallarg.substring(0,9);
+    var dia = diallarg.substring(0,10);
     var min = data["DailyForecasts"]["0"]["Temperature"]["Minimum"]["Value"];
     var max = data["DailyForecasts"]["0"]["Temperature"]["Maximum"]["Value"];
     var estatdia = data["DailyForecasts"]["0"]["Day"]["Icon"];
@@ -27,7 +27,7 @@ function weather() {
 
     document.getElementById("estatnit0").innerHTML = estatnit;
 
-    document.getElementById("fraseestatnit0").innerHTML = fraseestatnit0;
+    document.getElementById("fraseestatnit0").innerHTML = fraseestatnit;
 
     });
 
